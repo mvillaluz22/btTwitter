163db3fd447070e722965399af7b80d8bfc62bf3
Michael Villaluz <mvillaluz22@gmail.com>
2022-09-02 04:24:57 +0800

    bluetread

--- BlueTread/BlueTread.Web/Program.cs
+++ BlueTread/BlueTread.Web/Program.cs
@@ -8,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSpaStaticFiles(configuration =>
+{
+    configuration.RootPath = "ClientApp/dist/blue-tread-ui";
+});
 
 var app = builder.Build();
 
@@ -30,6 +34,14 @@ app.UseCors(builder =>
     .AllowAnyOrigin()
     .AllowAnyMethod()
     .AllowAnyHeader();
+});
+
+app.UseStaticFiles();
+app.UseSpaStaticFiles();
+
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = "ClientApp";
 });
 
 app.Run();
